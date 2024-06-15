a104a7e0d78f10b4e510daac0d9c468ab41aaee2
rittin2803 <rittin2803@gmail.com>
2024-06-15 13:02:57 +0530

    Completed Home page

--- src/components/HotelCard/HotelCard.jsx
+++ src/components/HotelCard/HotelCard.jsx
@@ -1,22 +1,25 @@
 import "./HotelCard.css"
 
-const HotelCard = () => {
+const HotelCard = ({hotel}) => {
+
+    const { _id, name, image, address, state, rating, price } = hotel;
+
     return (
         <div className="relative hotelcard-container shadow cursor-pointer">
             <div>
-                <img src="" alt="hotelcard" />
+                <img className="img" src={image} alt={name} />
                 <div className="hotelcard-details">
                     <div className="d-flex align-center">
-                        <span className="location"> Bir, Himachal Pradesh </span>
+                        <span className="location"> {address}, {state} </span>
                         <span className="rating d-flex align-center">
                             <span className="material-symbols-outlined">star</span>
-                            <span>4.3</span>
+                            <span>{rating}</span>
                         </span>
                     </div>
             
-                    <p className="hotel-name">Sukoon Bag</p>
+                    <p className="hotel-name">{name}</p>
                     <p className="price-details">
-                        <span className="price">Rs. 3500</span>
+                        <span className="price">Rs. {price}</span>
                         <span>/night</span>
                     </p>
                 </div>  
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,14 +1,33 @@
-import { Fragment } from "react";
+import { Fragment, useEffect, useState } from "react";
+import axios from "axios";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 import { Navbar, HotelCard } from "../../components";
 
 import "./Home.css";
 
 const Home = () => {
+
+    const [hotels, setHotels] = useState([]);
+
+    useEffect(() => {
+        ( async () => {
+            try{
+                const { data } = await axios.get("https://travel-app-backend-vo66.onrender.com/api/v1/hotels");
+                setHotels(data);
+            }catch(error){
+                console.log(error);
+            }
+        })()
+    }, [])
+
     return (
         <Fragment>
             <Navbar />
-            <main className="main">
-                <HotelCard />
+            <main className="main d-flex align-center wrap gap-larger">
+                {
+                    hotels && hotels.map(hotel => <HotelCard key = {hotel._id} hotel={hotel} />)
+                }
             </main>
         </Fragment>
     );
